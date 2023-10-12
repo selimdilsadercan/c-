f67d29f7b47de81ae85059debe89c4f10f79615c
selimdilsad <selimdilsadercan@gmail.com>
2023-10-12 11:04:31 +0300

    v4: changed to pointer constructor

--- L2/src/main.cpp
+++ L2/src/main.cpp
@@ -9,23 +9,23 @@ class Athlete {
     string name;
     int age;
     double time;
+
   public:
     Athlete(string name, int age, double time) {
         this->name = name;
         this->age = age;
         this->time = time;
     }
-    void printAthlette () {
+    void printAthlette() {
       cout << this->name << "-" << this->age << "-" << this->time << endl;
     }
 };
 
+////
 
 int main(){
   fstream newFile;
   newFile.open("athletes.txt", ios::in); //ios::in Open for input operations.
-
-  int *array = new int[3]; 
 
   string name;
   int age;
@@ -33,11 +33,24 @@ int main(){
 
   while(!newFile.eof()){ // eof = end of file
     newFile >> name >> age >> time;
-    Athlete an_athlette = Athlete(name, age, time);
-    an_athlette.printAthlette();
+    Athlete* an_athlette = new Athlete(name, age, time);
+    an_athlette->printAthlette();
   }
 
   newFile.close();
 
   return 0;
 }
+
+
+
+
+/*
+  POINTER
+  Athlete* an_athlette = new Athlete(name, age, time);
+  an_athlette->printAthlette();
+
+  NORMAL INSAN GİBİ
+  Athlete an_athlette = Athlete(name, age, time);
+  an_athlette.printAthlette();
+*/
